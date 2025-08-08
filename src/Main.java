a21f1784273885eadd27e911f125ffaeb01ebc89
GabrielMoreira17 <gabrielmoreira2128@gmail.com>
2025-08-07 21:08:39 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    /*
+    Objetivo: Calcular custo de uma excursão.
+
+    Variáveis sugeridas: double distanciaKm, double consumoKmPorLitro, double precoCombustivel.
+
+    Tarefas:
+
+    Guardar distância total (ida e volta).
+
+    Guardar consumo do ônibus.
+
+    Guardar preço do combustível.
+
+    Calcular litros necessários e custo total.
+     */
+    public static void main(String[] args) {
+
+        //Variavel que armazena a distancia
+        Double distancia;
+
+        //Variavel que armazena o consumo
+        Double ConsumoKmPorLitro;
+
+        //Variavel
+    }
+}
